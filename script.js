d7e448d03dd80359e741f42d08c69ddb9a4b616e
Nicolas Ferreras <61299305+nicoFerreras@users.noreply.github.com>
2023-04-13 13:31:24 -0300

    In JS i put three buttons to play with them, and a display that show the score of player an computer. In CSS i put some style, like color, background fonts etc.
    In HTML i only added the 3 buttons, and text for score.

--- script.js
+++ script.js
@@ -1,56 +1,58 @@
+
 let playerScore = 0
 let computerScore = 0
-let playerSelection = prompt("Choose...", '') /* Le pido al usuario un input */
 
 
-playRound(playerSelection); /*Llamo a la función*/
 
 function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
-  } /*Función para pedirle a la maquina que escoja un valor*/
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
 
-  function playRound(playerSelection) { /*Juego*/
-    let computerSelection = getComputerChoice()
-    let result = "";
 
-   if ((playerSelection == 'rock' && computerSelection == 'scissors') || /*Piedra papel o tijera, osea el juego*/
-        (playerSelection == 'paper' && computerSelection == 'rock') ||
-        (playerSelection == 'scissors' && computerSelection == 'paper')) {
 
-            playerScore += 1; /*Si el usuario gana se le suma un punto, esta acción dejara el mensaje de que gano, y cuanta puntuación tiene*/
-            result = `You win! ${playerSelection} beats ${computerSelection}. Now your score is: ${playerScore}, and the computer score is: ${computerScore}`
+function playRound(playerSelection) {
+  let computerSelection = getComputerChoice();
+  let result = "";
+  
 
-            if (playerScore == 5) { /*Si la puntuacion llega a 5 el usuario gana*/
-                result = "You won! congratulations, if you want to continue please reset the web."
-            }
-        } 
-        else if (playerSelection == computerSelection) /*Si los valores de la maquina y el usuario coinciden es un empate*/ {
-            result = ("Its a tie! ");
-        } 
-        else /*Mensaje en caso de que la maquina gane*/{
-            computerScore += 1
-            result = `You lose, your score is: ${playerScore} and the computer score is: ${computerScore} `
-            
-            if (computerScore == 5) /*Si la maquina llega a 5 gana*/
-            result = ("You lose the game, please reset the web if you want to continue")
-        }
-        alert(result);
-    } 
+  if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+      (playerSelection == 'paper' && computerSelection == 'rock') ||
+      (playerSelection == 'scissors' && computerSelection == 'paper')) {
 
-    while (playerScore < 5 && computerScore < 5) {
-        let playerSelection = prompt("Choose...", "");
-        playRound(playerSelection.toLowerCase());
+    playerScore += 1;
+    result = `You win! ${playerSelection} beats ${computerSelection}.`;
+
+    if (playerScore == 5) {
+      result = "You win! congratulations, if you want to continue please restart the web.";
+      playerScore = 0;
+      computerScore = 0;
     }
-    
-    /*Loop para que siga pidiendo un prompt hasta que alguno llegue a 5*/
+  } else if (playerSelection == computerSelection) {
+    result = "It's a tie!";
+  } else {
+    computerScore += 1;
+    result = `You lose, ${computerSelection} beat ${playerSelection}.`;
+
+    if (computerScore == 5) {
+      result = "You lose the game, please restart the web if you want to continue";
+      playerScore = 0;
+      computerScore = 0;
+    }
+  }
+
+  document.getElementById("result").textContent = result;
+  document.getElementById("leftScore").textContent = playerScore;
+  document.getElementById("rightScore").textContent = computerScore;
+}
 
 
+// An event listener to play with buttons
+const buttons = document.querySelectorAll('input')
+buttons.forEach((button) => {
+  button.addEventListener('click', function() {
+    playRound(button.value);
+  });
+});
 
-
-
-
-  
-
-  
